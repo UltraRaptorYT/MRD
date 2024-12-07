ce32034245735c4bf70b4b1c7514018c37331a6f
Soh Hong Yu <raymondng009@gmail.com>
2024-12-07 09:26:43 +0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -10,11 +10,6 @@ export default defineConfig({
       "@": path.resolve(__dirname, "./src"),
     },
   },
-  build: {
-    rollupOptions: {
-      external: ["@mediapipe/holistic"],
-    },
-  },
   esbuild: {
     keepNames: true,
   },
